348b0ee997862efabbb451babb67301230817246
iga1989 <112903582+iga1989@users.noreply.github.com>
2022-09-12 19:45:49 -0500

    Create app.js

---
+++ w2d1FuncProg/app.js
@@ -0,0 +1,4 @@
+let sum = (arr) => arr.reduce((acc, item) => acc + item, 0);
+let multiply = (arr) => arr.reduce((a, b) => a * b, 1);
+let reverse = (str) => [...str].reverse().join("");
+let filterLongWords = (words, i) => words.filter(word => word.length > i);
