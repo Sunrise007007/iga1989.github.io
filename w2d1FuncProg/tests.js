2681b048ac75a5ac221a1fae868455342a41c07f
iga1989 <112903582+iga1989@users.noreply.github.com>
2022-09-12 19:45:58 -0500

    Create tests.js

---
+++ w2d1FuncProg/tests.js
@@ -0,0 +1,21 @@
+
+describe("Sum", () => {
+    it("sum takes an array and returns the sum of all elements", () => {
+        assert.equal(15, sum([1, 2, 3, 4,5]));
+    });
+describe("Multiply", () => {
+    it("multiply takes an array and returns the product of all elements", () => {
+        assert.equal("hataner", reverse("renatah"));
+        });
+    });
+});
+describe("Reverse", () => {
+    it("reverse takes in a string and returns its reversal", () => {
+        assert.equal(120, multiply([1, 2, 3, 4,5]));
+    });
+});
+describe("FilterLongWords", () => {
+    it("filterLongWords takes an array of words and an integer i and returns the array of words that are longer than i.", () => {
+        expect(["ritah", "renatah"]).to.eql(filterLongWords(["ritah", "renatah", "iga", "dan"], 3));
+    });
+});
