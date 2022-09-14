24dc45877c78acc21b88671182fd53753c978092
iga1989 <112903582+iga1989@users.noreply.github.com>
2022-09-13 23:23:32 -0500

    Update counter.js

--- w2d2/counter.js
+++ w2d2/counter.js
@@ -20,10 +20,30 @@ const count = (function () {
   })();
   
   console.log(count.value()); // 0.
-  
   count.add();
   count.add();
   console.log(count.value()); // 2.
-  
   count.reset();
   console.log(count.value()); // 1.
+
+
+var add = (function(){
+    var counter = 0;
+    return function(){
+        return counter += 1;
+    }
+})();
+add();
+console.log(add());
+
+var make_adder = function (inc) {
+    var counter = 0;
+    return function() {
+        return counter += inc;
+    };
+};
+var add5 = make_adder(5);
+add5(); add5(); console.log(add5());
+
+var add7 = make_adder(7);
+add7(); add7(); console.log(add7());
